7048099db439031630bc3adfc199d749672f282e
fabiopadua <fabio.padua@luizalabs.com>
2021-04-26 16:53:30 -0300

    chore: comments removal

--- api/v1/stockmarket/stockmarket-routes.js
+++ api/v1/stockmarket/stockmarket-routes.js
@@ -9,23 +9,3 @@ module.exports = function(app) {
     return Controller.getStockMarketShare(req, res);
   });
 }
-
-// exports.register = (server, options, next) => {
-//   server.route([{
-//     method: 'GET',
-//     path: `${basePath}/{stockCode}`,
-//     config: {
-//       description: 'v1 - Get stockmarket share value',
-//       tags: ['api'],
-//       handler: Controller.getStockMarketShare,
-//       validate: Validator.queryParamStockMarketShare,
-//       notes: 'v1_get_stockmarket'
-//     }
-//   }]);
-//   next();
-// };
-
-// exports.register.attributes = {
-//   name: 'v1-stock-market-share',
-//   version: '1.0.0'
-// };
